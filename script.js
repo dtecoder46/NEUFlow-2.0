eaaa84ed90003e4a23d64dc3c1c1fdde6c70cfe2
Daniel <122654917+dtecoder46@users.noreply.github.com>
2024-07-17 13:09:28 -0400

    Added call to C++ server

---
+++ script.js
@@ -0,0 +1,19 @@
+import { exec } from 'child_process';
+
+exec('g++ -o main main.cpp',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+
+exec('./main',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
