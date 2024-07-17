b6b7dd64a2c6eef08a4041cf313ee30e45d805dd
Daniel <122654917+dtecoder46@users.noreply.github.com>
2024-07-17 13:18:00 -0400

    Added connection to C++ server

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { exec } from 'child_process';
+
 const app = Vue.createApp({
   data() {
     return {
@@ -14,3 +16,21 @@ const app = Vue.createApp({
 })
 
 app.mount('#app')
+
+exec('g++ -o main main.cpp',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+
+exec('./main',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
