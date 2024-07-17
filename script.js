078f82856e0f778c1c8d0c7a11c8a0cfff289660
Daniel <122654917+dtecoder46@users.noreply.github.com>
2024-07-17 18:53:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 import { exec } from 'child_process';
 
-const app = Vue.createApp({
-  data() {
-    return {
-      message: "Hello World!",
-      logo: "logo.png",
-      menu: "menu_button.png",
-      box: "box.png",
-      custom: "custom.jpg",
-      money: "money.jpg",
-      gmail: "gmail.png",
-      twitter: "twitter.png",
-    }
-  }
-})
-
-app.mount('#app')
-
 exec('g++ -o main main.cpp',
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
